c64f32449148971d44e4f8a6f8b8058b8224972b
Phyo Wai Nyunt <pwn@Phyos-MacBook-Air.local>
2022-09-15 15:02:53 +0630

    Navbar implement | SearchBar added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';    // v18 change
 
 import App from './App';
-import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
