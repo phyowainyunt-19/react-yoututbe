cb569ec3f6329669808cf85f86490ea1437d3311
phyowainyunt-19 <phyowainyunt.26@gmail.com>
2022-09-18 23:19:55 +0630

    Youtube api key added and fetchApi function integrated

---
+++ src/utils/fetchApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const BASE_YT_URL = "https://youtube-v31.p.rapidapi.com/search";
+
+const options = {
+    url: BASE_YT_URL,
+    params: {
+        // relatedToVideoId: '7ghhRHRP6t4',
+        // part: 'id,snippet',
+        // type: 'video',
+        maxResults: '50'
+    },
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+};
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get(`${BASE_YT_URL}/${url}`, options);
+
+    return data;
+}
