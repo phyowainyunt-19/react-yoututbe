3766e2ee025f7c2e29c1be691bc8418ec56ac8d0
phyowainyunt-19 <phyowainyunt.26@gmail.com>
2022-09-25 23:22:16 +0630

    Project reviewed

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
+import { demoProfilePicture } from '../utils/constants';
+// project reviewed
 const ChannelCard = () => {
   return (
     <div>ChannelCard</div>
