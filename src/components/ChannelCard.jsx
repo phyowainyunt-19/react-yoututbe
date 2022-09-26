a3e68384305c63c790a60b045540a2ca21b17a0e
phyowainyunt-19 <phyowainyunt.26@gmail.com>
2022-09-26 13:22:37 +0630

    ChannelCard cardmedia integrated

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,10 +3,28 @@ import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
 import { demoProfilePicture } from '../utils/constants';
-// project reviewed
-const ChannelCard = () => {
+
+const ChannelCard = ({ channelDetail }) => {
+  console.log(channelDetail)
   return (
-    <div>ChannelCard</div>
+    <Box
+      sx={{ boxShadow: 'none', borderRadius: '20px' }}
+    >
+      {/* Click the link and go directly into the channel */}
+      <Link to={`/channel/${channelDetail?.id?.channelIdf}`}>
+        <CardContent
+          sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}
+        >
+          <CardMedia
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: '50%', height: '180px', width: '180px'  
+            }}
+          />
+        </CardContent>
+      </Link>
+    </Box>
   )
 }
 
