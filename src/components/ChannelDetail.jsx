995a0269ac3d5937458f83643e94961e4e30d57f
phyowainyunt-19 <phyowainyunt.26@gmail.com>
2022-09-28 14:31:59 +0630

    ChannelDetail component updated

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -8,18 +8,26 @@ import { fetchApi } from '../utils/fetchApi'
 const ChannelDetail = () => {
 
   const [channelDetail, setChannelDetail] = useState(null);
+  const [channelVideos, setChannelVideos] = useState([]);
   const { id } = useParams();
 
-  console.log(channelDetail)
+  // console.log(channelDetail, channelVideos)
+  
   // fetch data and set ChannelDetail page
   useEffect(() => {
-    fetchApi(`channels?part="snippet"&id=${id}`)
+    fetchApi(`channels?part=snippet&id=${id}`)
       .then((data) => setChannelDetail(data?.items[0]));
+
+    // fetch the videos from the channel
+    fetchApi(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setChannelVideos(data?.items));
   }, [id])
 
 
   return (
-    <div>{id}</div>
+    <Box>
+      
+    </Box>
   )
 }
 
