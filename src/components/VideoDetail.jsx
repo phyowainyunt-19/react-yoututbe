0db23ad9c2de7a7be5e5f97f084e25a49d365cc4
phyowainyunt-19 <phyowainyunt.26@gmail.com>
2022-10-01 22:34:03 +0630

    Project reviewed

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Box, Stack, Typography } from "@mui/material";
+import ReactPlayer from "react-player";
+import { CheckCircle } from "@mui/icons-material";
+
+import { Videos } from './';
+import { fetchApi } from "../utils/fetchApi";
+
 
 const VideoDetail = () => {
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+
+    </Box>
   )
 }
 
